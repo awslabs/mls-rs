b58f79563b04f535bfabd103ea7747109b54d1c5
Stephane Raux <sraux@amazon.com>
2021-12-14 14:36:42 -0600

    Improve key serialization benchmark code

--- benches/tls.rs
+++ benches/tls.rs
@@ -1,20 +1,34 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
 use ferriscrypt::asym::ec_key::{Curve, SecretKey};
+use std::{io, iter};
 use tls_codec::Serialize;
-use tls_codec_derive::{TlsDeserialize, TlsSerialize, TlsSize};
+use tls_codec_derive::{TlsSerialize, TlsSize};
 use wickr_bgm as mls;
 
-#[derive(Debug, TlsDeserialize, TlsSerialize, TlsSize)]
-struct Keys(#[tls_codec(with = "mls::tls::Vector::<u32, mls::tls::SecretKeySer>")] Vec<SecretKey>);
+#[derive(Debug, TlsSerialize, TlsSize)]
+struct KeyRefs<'a>(
+    #[tls_codec(with = "mls::tls::Vector::<u32, mls::tls::SecretKeySer>")] &'a [SecretKey],
+);
 
 fn serialize_keys(c: &mut Criterion) {
-    c.bench_function("serialize_keys", |b| {
-        b.iter(|| {
-            let _ = black_box(Keys(vec![SecretKey::generate(Curve::Ed25519).unwrap()]))
-                .tls_serialize_detached()
-                .unwrap();
-        })
-    });
+    let keys = iter::repeat_with(|| SecretKey::generate(Curve::Ed25519).unwrap())
+        .take(10_000)
+        .collect::<Vec<_>>();
+    let mut bench_group = c.benchmark_group("serialize_keys");
+    let counts = iter::once(0)
+        .chain(iter::successors(Some(1), |&n| Some(10 * n)))
+        .take_while(|&n| n <= keys.len());
+    for n in counts {
+        bench_group.throughput(Throughput::Elements(n as u64));
+        bench_group.bench_with_input(BenchmarkId::from_parameter(n), &n, |b, &n| {
+            b.iter(|| {
+                let _ = black_box(KeyRefs(&keys[..n]))
+                    .tls_serialize(&mut io::sink())
+                    .unwrap();
+            });
+        });
+    }
+    bench_group.finish();
 }
 
 criterion_group!(benches, serialize_keys);
