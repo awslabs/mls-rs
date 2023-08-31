8fa04281aed40494831f1bde5cbc79dfdac4f117
Stephane Raux <94983192+stefunctional@users.noreply.github.com>
2023-08-31 13:45:41 -0500

    Specialize Client and Group using OpenSSL and SQLite for FFI (#877)
    The next step will be to provide the C equivalent of ClientBuilder.

--- aws-mls-ffi/src/lib.rs
+++ aws-mls-ffi/src/lib.rs
@@ -1,13 +1,26 @@
-use aws_mls::{
-    client_builder::{BaseConfig, WithCryptoProvider, WithIdentityProvider},
-    identity::basic::BasicIdentityProvider,
-};
-use aws_mls_crypto_openssl::OpensslCryptoProvider;
+#[cfg(all(feature = "openssl", feature = "sqlite", feature = "x509"))]
+mod openssl_sqlite {
+    use aws_mls::client_builder::{BaseConfig, WithCryptoProvider, WithIdentityProvider};
+    use aws_mls_crypto_openssl::{
+        x509::{X509Reader, X509Validator},
+        OpensslCryptoProvider,
+    };
+    use aws_mls_identity_x509::{
+        NoOpWarningProvider, SubjectIdentityExtractor, X509IdentityProvider,
+    };
 
-pub type FfiConfig = WithIdentityProvider<
-    BasicIdentityProvider,
-    WithCryptoProvider<OpensslCryptoProvider, BaseConfig>,
->;
+    pub type OpensslSqlMlsConfig = WithIdentityProvider<
+        X509IdentityProvider<
+            SubjectIdentityExtractor<X509Reader>,
+            X509Validator,
+            NoOpWarningProvider,
+        >,
+        WithCryptoProvider<OpensslCryptoProvider, BaseConfig>,
+    >;
 
-safer_ffi_gen::specialize!(FfiClient = aws_mls::client::Client<FfiConfig>);
-safer_ffi_gen::specialize!(FfiGroup = aws_mls::group::Group<FfiConfig>);
+    safer_ffi_gen::specialize!(OpensslSqlMlsClient = aws_mls::client::Client<OpensslSqlMlsConfig>);
+    safer_ffi_gen::specialize!(OpensslSqlMlsGroup = aws_mls::group::Group<OpensslSqlMlsConfig>);
+}
+
+#[cfg(all(feature = "openssl", feature = "sqlite", feature = "x509"))]
+pub use openssl_sqlite::*;
