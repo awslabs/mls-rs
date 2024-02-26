c5ccc9f3dd455f5b90dac709980ba64209d6da2d
Marta Mularczyk <mulmarta@amazon.com>
2024-02-26 18:53:47 +0100

    Add more Python FFI examples

--- mls-rs-uniffi/src/lib.rs
+++ mls-rs-uniffi/src/lib.rs
@@ -591,4 +591,64 @@ assert signature_keypair.cipher_suite == CipherSuite.CURVE25519_AES128
 "#,
         )
     }
+
+    #[test]
+    fn test_simple_scenario() -> Result<(), Box<dyn std::error::Error>> {
+        run_python(
+            r#"
+from mls_rs_uniffi import CipherSuite, generate_signature_keypair, Client
+
+key = generate_signature_keypair(CipherSuite.CURVE25519_AES128)
+alice = Client(b'alice', key)
+
+key = generate_signature_keypair(CipherSuite.CURVE25519_AES128)
+bob = Client(b'bob', key)
+
+alice = alice.create_group(None)
+kp = bob.generate_key_package_message()
+commit = alice.add_members([kp])
+alice.process_incoming_message(commit.commit_message())
+bob = bob.join_group(commit.welcome_messages()[0]).group
+
+msg = alice.encrypt_application_message(b'hello, bob')
+output = bob.process_incoming_message(msg)
+
+assert output.data == b'hello, bob'
+"#,
+        )
+    }
+}
+
+// TODO(mulmarta): it'll break if we use async trait which will be supported in
+// the next UniFFI release
+#[cfg(all(test, mls_build_async))]
+mod async_tests {
+    use crate::test_utils::run_python;
+
+    #[test]
+    fn test_simple_scenario() -> Result<(), Box<dyn std::error::Error>> {
+        run_python(
+            r#"
+from mls_rs_uniffi import CipherSuite, generate_signature_keypair, Client
+from asyncio import run
+
+key = run(generate_signature_keypair(CipherSuite.CURVE25519_AES128))
+alice = Client(b'alice', key)
+
+key = run(generate_signature_keypair(CipherSuite.CURVE25519_AES128))
+bob = Client(b'bob', key)
+
+alice = run(alice.create_group(None))
+kp = run(bob.generate_key_package_message())
+commit = run(alice.add_members([kp]))
+run(alice.process_incoming_message(commit.commit_message()))
+bob = run(bob.join_group(commit.welcome_messages()[0])).group
+
+msg = run(alice.encrypt_application_message(b'hello, bob'))
+output = run(bob.process_incoming_message(msg))
+
+assert output.data == b'hello, bob'
+"#,
+        )
+    }
 }
