aca74a7d5de3514b067e248cf1fa2934a12147fc
Martin Geisler <mgeisler@google.com>
2024-04-23 00:54:50 +0200

    [uniffi] Auto-download Kotlin dependencies (#149)
    This makes `cargo test` automatically download the necessary jar files
    used by the test code. The jars are cached in `$CARGO_TARGET_TMPDIR`,
    i.e., in `target/tmp`.
    
    Co-authored-by: mulmarta <103590845+mulmarta@users.noreply.github.com>

--- mls-rs-uniffi/tests/kotlin_scenarios.rs
+++ mls-rs-uniffi/tests/kotlin_scenarios.rs
@@ -2,6 +2,78 @@
 // run on the Windows runner in GitHub's CI. See #133.
 #![cfg(unix)]
 
+use anyhow::Context;
+use std::path::{Path, PathBuf};
+
+struct MavenArtifact {
+    group_id: String,
+    artifact_id: String,
+    version: String,
+}
+
+impl MavenArtifact {
+    fn new(group_id: &str, artifact_id: &str, version: &str) -> Self {
+        Self {
+            group_id: String::from(group_id),
+            artifact_id: String::from(artifact_id),
+            version: String::from(version),
+        }
+    }
+
+    fn download(&self) -> anyhow::Result<PathBuf> {
+        let MavenArtifact {
+            group_id,
+            artifact_id,
+            version,
+        } = self;
+        let output_dir = env!("CARGO_TARGET_TMPDIR");
+        let jar_path = format!("{artifact_id}-{version}.jar");
+        let download_path = Path::new(output_dir).join(jar_path);
+
+        if download_path.exists() {
+            eprintln!("Found {}, skipping Maven download", download_path.display());
+            return Ok(download_path);
+        }
+
+        eprintln!("Downloading {} using Maven", download_path.display());
+        let exit_status = std::process::Command::new("mvn")
+            .arg("--no-transfer-progress")
+            .arg("dependency:copy")
+            .arg(&format!("-Dartifact={group_id}:{artifact_id}:{version}"))
+            .arg(format!("-DoutputDirectory={output_dir}"))
+            .status()
+            .context("running `mvn` failed")?;
+        if !exit_status.success() {
+            anyhow::bail!("Error while running mvn!");
+        }
+
+        Ok(download_path)
+    }
+}
+
+/// Download and configure Kotlin test dependencies.
+///
+/// This will download the Kotlin test dependencies to
+/// `$CARGO_TARGET_TMPDIR` and globally update the `$CLASSPATH`
+/// environment variable to point to them.
+fn configure_kotlin_env() -> anyhow::Result<()> {
+    let deps = [
+        MavenArtifact::new("net.java.dev.jna", "jna", "5.14.0"),
+        MavenArtifact::new("org.jetbrains.kotlin", "kotlin-test", "1.9.23"),
+    ];
+
+    let classpath = std::env::var("CLASSPATH").unwrap_or_default();
+    let mut classpaths = std::env::split_paths(&classpath).collect::<Vec<_>>();
+    for dep in deps {
+        let dep_path = dep.download()?;
+        if !classpaths.contains(&dep_path) {
+            classpaths.push(dep_path);
+        }
+    }
+    std::env::set_var("CLASSPATH", std::env::join_paths(classpaths)?);
+    Ok(())
+}
+
 macro_rules! generate_kotlin_tests {
     ($sync_scenario:ident, None) => {
         #[cfg(not(mls_build_async))]
@@ -23,7 +95,8 @@ macro_rules! generate_kotlin_tests {
 
     ($scenario:ident) => {
         #[test]
-        fn $scenario() -> Result<(), Box<dyn std::error::Error>> {
+        fn $scenario() -> anyhow::Result<()> {
+            configure_kotlin_env()?;
             let target_dir = env!("CARGO_TARGET_TMPDIR");
             let script_path = format!("tests/{}.kts", stringify!($scenario));
             uniffi_bindgen::bindings::kotlin::run_script(
