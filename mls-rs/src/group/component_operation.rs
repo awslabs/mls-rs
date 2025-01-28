271fe27c753b71981a1e58cedae7b51a12b5ac83
Ellen Arteca <emarteca@google.com>
2025-01-28 13:44:22 -0800

    fixing componentoperation

--- mls-rs/src/group/component_operation.rs
+++ mls-rs/src/group/component_operation.rs
@@ -1,23 +1,17 @@
 use crate::client::MlsError;
-use crate::tree_kem::hpke_encryption::HpkeEncryptable;
 use mls_rs_codec::{MlsDecode, MlsEncode, MlsSize};
 
 pub type ComponentID = u32;
 
 #[derive(Clone, Debug, PartialEq, MlsSize, MlsEncode, MlsDecode)]
 pub struct ComponentOperationLabel {
+    label: Vec<u8>,
     component_id: ComponentID,
     context: Vec<u8>,
 }
 
-impl HpkeEncryptable for ComponentOperationLabel {
-    const ENCRYPT_LABEL: &'static str = "MLS 1.0 Application";
-
-    fn from_bytes(bytes: Vec<u8>) -> Result<Self, MlsError> {
-        Self::mls_decode(&mut bytes.as_slice()).map_err(Into::into)
-    }
-
-    fn get_bytes(&self) -> Result<Vec<u8>, MlsError> {
+impl ComponentOperationLabel {
+    pub fn get_bytes(&self) -> Result<Vec<u8>, MlsError> {
         self.mls_encode_to_vec().map_err(Into::into)
     }
 }
@@ -25,6 +19,7 @@ impl HpkeEncryptable for ComponentOperationLabel {
 impl ComponentOperationLabel {
     pub fn new(component_id: u32, context: Vec<u8>) -> Self {
         Self {
+            label: b"MLS 1.0 Application".to_vec(),
             component_id,
             context,
         }
