fea8f442a9bd582b02ce7a7eb56056093da42aa6
Marta Mularczyk <mulmarta@amazon.ch>
2026-07-26 19:15:57 +0200

    apply clippy suggestion

--- mls-rs-crypto-openssl/src/ec.rs
+++ mls-rs-crypto-openssl/src/ec.rs
@@ -454,7 +454,8 @@ mod tests {
             {
                 println!(
                     "Mismatched curve public key import : key curve {:?}, import curve {:?}",
-                    &curve, &other_curve
+                    curve,
+                    other_curve
                 );
 
                 let public_key = get_test_public_keys().get_key_from_curve(curve);
